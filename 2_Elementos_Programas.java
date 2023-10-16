2b0036db6a9a3083347ae419ad7e07dea1540809
danstort <129515419+danstort@users.noreply.github.com>
2023-10-16 12:34:27 +0200

    Create 2_Elementos_Programas.java

---
+++ 2_Elementos_Programas.java
@@ -0,0 +1,8 @@
+public class Unidad2Proyecto202324 {
+
+    public static void main(String[] args) {
+        int numero = 2, cuad;
+        cuad = numero * numero;
+        System.out.println("EL CUADRADO DE " + numero + " ES: " + cuad);
+    }
+}
