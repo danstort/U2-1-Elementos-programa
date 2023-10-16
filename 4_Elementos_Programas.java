e8ec4453fc344c49318fc511ecf0a97c637731a3
danstort <129515419+danstort@users.noreply.github.com>
2023-10-16 13:31:01 +0200

    Create 4_Elementos_Programas.java

---
+++ 4_Elementos_Programas.java
@@ -0,0 +1,10 @@
+public class Area {
+
+    public static void main(String[] args) {
+        double rad = 5.2;
+        double res = Math.PI*Math.pow(rad, 2);
+        
+                
+        System.out.println("El area de la circunferenca de radio 5.2 m es: "+res);
+    }
+}
